543592fe6b7a41be58e15c09b05c7846c921a530
utkarsh <singhrishu680@gmail.com>
2023-04-30 20:04:47 +0530

    done with handleInputKeypress

---
+++ todo.js
@@ -0,0 +1,48 @@
+const tasks = []
+
+const taskList = document.getElementById('list');
+const addTaskInput = document.getElementById('add');
+const taskCounter = document.getElementById('tasks-counter');
+
+console.log(taskCounter);
+
+function renderList () {
+
+
+}
+
+function markTaskCompleted (taskId) {}
+
+function deleteTask (taskId) {
+    
+}
+
+function addTask (task) {
+    
+
+}
+
+function showNotification (text) {
+    alert(text);
+}
+
+function handleInputKeypress (e) {
+    if(e.key === 'Enter') {
+        const text = e.target.value;
+        console.log('text', text);
+
+        if(!text){
+            showNotification('task cant be empty');
+            return;
+        }
+        const task = {
+            text,
+            id: Date.now().toString(),
+            done:false
+        }
+        e.target.value = '';
+        addTask(task);
+        }
+    }
+
+addTaskInput.addEventListener('keyup', handleInputKeypress);
